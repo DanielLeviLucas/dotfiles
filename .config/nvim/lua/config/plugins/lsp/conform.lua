eba410f50cfd891bcdf90aca093c7bf78d370337
daniel <daniellevilucas@gmail.com>
2024-04-03 00:57:31 +0530

    nvim: Add formatter plugin for Neovim

---
+++ .config/nvim/lua/config/plugins/lsp/conform.lua
@@ -0,0 +1,36 @@
+return {
+    {
+        "stevearc/conform.nvim",
+        event = { "BufReadPre", "BufNewFile" },
+        cmd = { "ConformInfo" },
+        keys = {
+            {
+                "<leader>cf",
+                function()
+                    require("conform").format({ async = true, lsp_fallback = true })
+                end,
+                mode = "",
+                desc = "Format buffer",
+            },
+        },
+        opts = {
+            formatters_by_ft = {
+                lua = { "stylua" },
+                -- Conform can also run multiple formatters sequentially
+                python = { "autoflake", "isort", "autopep8" },
+                --
+                -- You can use a sub-list to tell conform to run *until* a formatter
+                -- is found.
+                svelte = { { "prettierd", "prettier" } },
+                javascript = { { "prettierd", "prettier" } },
+                typescript = { { "prettierd", "prettier" } },
+                javascriptreact = { { "prettierd", "prettier" } },
+                typescriptreact = { { "prettierd", "prettier" } },
+                json = { { "prettierd", "prettier" } },
+                css = { { "prettierd", "prettier" } },
+                scss = { { "prettierd", "prettier" } },
+                proto = { "buf" },
+            },
+        },
+    },
+}
