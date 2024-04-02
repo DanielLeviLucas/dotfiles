bd5577391944267c61c1c51257b801ea13ebd76b
daniel <daniellevilucas@gmail.com>
2024-04-02 21:29:17 +0530

    refactor(nvim): Use standard API to set keymaps
    - includes new telescope keymaps to fuzzy find
        - [S]earch Recent Files
        - Fuzzily search in current buffer
        - [S]earch [/] in Open Files

--- .config/nvim/lua/config/plugins/telescope.lua
+++ .config/nvim/lua/config/plugins/telescope.lua
@@ -66,59 +66,46 @@ return {
                 },
             }
             -- Enable telescope fzf native, if installed
-            pcall(require('telescope').load_extension, 'fzf')
+            pcall(require("telescope").load_extension, "fzf")
             require("telescope").load_extension("fzf")
+
             local builtin = require("telescope.builtin")
-            vim.keymap.set(
-                "n", "<leader>pws",
-                function()
-                    local word = vim.fn.expand("<cword>")
-                    builtin.grep_string({ search = word })
-                end,
-                { desc = "Search current hightlight word" }
-            )
-            vim.keymap.set(
-                "n", "<leader>pWs",
-                function()
-                    local word = vim.fn.expand("<cWORD>")
-                    builtin.grep_string({ search = word })
-                end,
-                { desc = "Search current hightlight whole word" }
-            )
-        end,
-        keys = function()
-            return {
-                {
-                    "<leader>pf",
-                    "<cmd>Telescope find_files<cr>",
-                    mode = { "n" },
-                    desc = "Find Files",
-                },
-                {
-                    "<leader>ps",
-                    "<cmd>Telescope live_grep<cr>",
-                    mode = { "n" },
-                    desc = "Search for a string and get results live as you type, respects .gitignore",
-                },
-                {
-                    "<leader>ph",
-                    "<cmd>Telescope help_tags<cr>",
-                    mode = { "n" },
-                    desc = "Lists available help tags and opens a new window with the relevant help",
-                },
-                {
-                    "<leader>pd",
-                    "<cmd>Telescope diagnostics<cr>",
-                    mode = { "n" },
-                    desc = "[S]earch [D]iagnostics",
-                },
-                {
-                    "<C-p>",
-                    "<cmd>Telescope git_files<cr>",
-                    mode = { "n" },
-                    desc = "git_files",
-                },
-            }
+
+            vim.keymap.set("n", "<leader>pws", function()
+                local word = vim.fn.expand("<cword>")
+                builtin.grep_string({ search = word })
+            end, { desc = "Search current hightlight word" })
+
+            vim.keymap.set("n", "<leader>pWs", function()
+                local word = vim.fn.expand("<cWORD>")
+                builtin.grep_string({ search = word })
+            end, { desc = "Search current hightlight whole word" })
+
+            vim.keymap.set("n", "<leader>ph", builtin.help_tags, { desc = "[S]earch [H]elp" })
+            vim.keymap.set("n", "<leader>pk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
+            vim.keymap.set("n", "<leader>pf", builtin.find_files, { desc = "[S]earch [F]iles" })
+            vim.keymap.set("n", "<leader>ps", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+            vim.keymap.set("n", "<leader>pd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
+            vim.keymap.set("n", "<leader>p.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+            vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
+            vim.keymap.set("n", "<C-p>", builtin.git_files, { desc = "[S]earch Git Files" })
+
+            vim.keymap.set("n", "<leader>/", function()
+                -- You can pass additional configuration to Telescope to change the theme, layout, etc.
+                builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+                    winblend = 10,
+                    previewer = false,
+                }))
+            end, { desc = "[/] Fuzzily search in current buffer" })
+
+            -- It's also possible to pass additional configuration options.
+            --  See `:help telescope.builtin.live_grep()` for information about particular keys
+            vim.keymap.set("n", "<leader>p/", function()
+                builtin.live_grep({
+                    grep_open_files = true,
+                    prompt_title = "Live Grep in Open Files",
+                })
+            end, { desc = "[S]earch [/] in Open Files" })
         end,
     },
 }
