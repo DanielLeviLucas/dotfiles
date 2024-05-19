bb7b0d2b265ef2127fd391281edc3f387f3c389c
daniel <daniellevilucas@gmail.com>
2024-05-19 15:21:01 +0530

    Add cloak plugin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -316,6 +316,33 @@ local plugins = {
     end,
   },
   -- }}}
+  -- Cloak - Cloak you to overlay *'s (any character) over defined patterns in defined files{{{
+  {
+    "laytan/cloak.nvim",
+    config = function()
+      require("cloak").setup({
+        enabled = true,
+        cloak_character = "*",
+        -- The applied highlight group (colors) on the cloaking, see `:h highlight`.
+        highlight_group = "Comment",
+        patterns = {
+          {
+            -- Match any file starting with ".env".
+            -- This can be a table to match multiple file patterns.
+            file_pattern = {
+              ".env*",
+              "wrangler.toml",
+              ".dev.vars",
+            },
+            -- Match an equals sign and any character after it.
+            -- This can also be a table of patterns to cloak,
+            -- example: cloak_pattern = { ":.+", "-.+" } for yaml files.
+            cloak_pattern = "=.+",
+          },
+        },
+      })
+    end,
+  },-- }}}
   -- Comment - Smart and Powerful commenter {{{
   {
     "numToStr/Comment.nvim",
