c4159591b00472e013a85743bd1514285dc3c837
Daniel Sam <daniellevilucas@gmail.com>
2024-08-19 13:50:59 +0530

    nvim(Comment): Update configuration with additional mappings and options
    - Added extra keybindings for commenting above, below, and at end of line

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -349,15 +349,34 @@ local plugins = {
     "numToStr/Comment.nvim",
     config = function()
       require("Comment").setup({
+        padding = true,
+        sticky = true,
         ignore = "^$",
         toggler = {
           line = "<leader>cc",
           block = "<leader>bc",
         },
         opleader = {
-          line = "<leader>c",
-          block = "<leader>b",
+          line = "gc",
+          block = "gb",
         },
+        extra = {
+          above = "gcO",
+          below = "gco",
+          eol = "gcA",
+        },
+        ---Enable keybindings
+        ---NOTE: If given `false` then the plugin won't create any mappings
+        mappings = {
+          ---Operator-pending mapping; `gcc` `gbc` `gc[count]{motion}` `gb[count]{motion}`
+          basic = true,
+          ---Extra mapping; `gco`, `gcO`, `gcA`
+          extra = true,
+        },
+        ---Function to call before (un)comment
+        pre_hook = nil,
+        ---Function to call after (un)comment
+        post_hook = nil,
       })
     end,
   },
