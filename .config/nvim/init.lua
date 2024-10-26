9433eb9834418a2a874be8b6ddbe76fd110e71af
Daniel S A <daniellevilucas@gmail.com>
2024-10-27 01:17:03 +0530

    nvim(Conform): Integrate autoflake for Python formatting

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -270,7 +270,7 @@ local plugins = {
       formatters_by_ft = {
         lua = { "stylua" },
         -- Conform can also run multiple formatters sequentially
-        python = { "isort", "ruff_format" },
+        python = { "autoflake", "isort", "ruff_format" },
         --
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
