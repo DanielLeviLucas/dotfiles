dc800bc6287a79ca502ace189b80a560705dcd09
Daniel S A <daniellevilucas@gmail.com>
2025-01-17 00:00:00 +0530

    Daily driving Ghostty ♥  Hahimoto

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -15,7 +15,7 @@ from qtile_extras.widget.decorations import BorderDecoration
 HOME_DIR = os.path.expanduser('~')
 
 mod = "mod4"              # Sets mod key to SUPER/WINDOWS
-myTerm = "kitty"          # My terminal of choice
+myTerm = "ghostty"        # My terminal of choice
 
 
 keys = [
