6bd89800bf986cd3883ecf606f7d2bc1b116107e
Daniel S A <daniellevilucas@gmail.com>
2025-07-13 21:54:48 +0530

    nvim(telescope): Use master branch instead of 0.1.x

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1130,7 +1130,6 @@ local plugins = {
   -- Telescope - Find, Filter, Preview, Pick. All lua, all the time {{{
   {
     "nvim-telescope/telescope.nvim",
-    branch = "0.1.x",
     dependencies = {
       { "nvim-lua/plenary.nvim" },
       { "nvim-telescope/telescope-ui-select.nvim" },
