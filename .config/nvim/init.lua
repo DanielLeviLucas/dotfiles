bcab9994b1f0742ee6a3451f0de39e5a5d240fb8
Daniel S A <daniellevilucas@gmail.com>
2026-03-30 07:32:00 +0530

    nvim(treesitter): Switch to new treesitter rewrite

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1328,41 +1328,42 @@ local plugins = {
   },
   -- }}}
   -- Treesitter {{{
-  {
+  { -- Highlight, edit, and navigate code
     "nvim-treesitter/nvim-treesitter",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter-textobjects",
-    },
+    lazy = false,
     build = ":TSUpdate",
-    opts = {
-      ensure_installed = {
-        "bash",
-        "html",
-        "css",
-        "json",
-        "javascript",
-        "toml",
-        "markdown",
-        "yaml",
-        "typescript",
-        "go",
-        "python",
-        "c",
-        "lua",
-        "vim",
-        "rust",
-      },
-      -- Autoinstall languages that are not installed
-      auto_install = true,
-      highlight = {
-        enable = true,
-        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-        --  If you are experiencing weird indenting issues, add the language to
-        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { "ruby" },
-      },
-      indent = { enable = true, disable = { "ruby" } },
-    },
+    branch = "main",
+    -- [[ Configure Treesitter ]] See `:help nvim-treesitter-intro`
+    config = function()
+      local parsers =
+        { "bash", "c", "diff", "html", "lua", "luadoc", "markdown", "markdown_inline", "query", "vim", "vimdoc" }
+      require("nvim-treesitter").install(parsers)
+      vim.api.nvim_create_autocmd("FileType", {
+        callback = function(args)
+          local buf, filetype = args.buf, args.match
+
+          local language = vim.treesitter.language.get_lang(filetype)
+          if not language then
+            return
+          end
+
+          -- check if parser exists and load it
+          if not vim.treesitter.language.add(language) then
+            return
+          end
+          -- enables syntax highlighting and other treesitter features
+          vim.treesitter.start(buf, language)
+
+          -- enables treesitter based folds
+          -- for more info on folds see `:help folds`
+          -- vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+          -- vim.wo.foldmethod = 'expr'
+
+          -- enables treesitter based indentation
+          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end,
+      })
+    end,
   },
   -- }}}
   -- Undotree - The undo history visualizer {{{
