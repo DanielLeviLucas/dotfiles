234d21cc59cd41621a67f1621d9763010aaf88bd
Daniel S A <daniellevilucas@gmail.com>
2026-04-03 20:20:32 +0530

    qtile(autostart): Use feh as bg setting replacing nitrogen

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -2,7 +2,7 @@
 picom &
 conky -c $HOME/.config/conky/qtile/dracula_theme.conkyrc &
 dunst &
-nitrogen --restore &
+feh --bg-scale "$HOME/Wallpapers/Itachi Uchiha Dark Raven.jpg" &
 copyq &
 setxkbmap us dvorak &
 xset -dpms &
